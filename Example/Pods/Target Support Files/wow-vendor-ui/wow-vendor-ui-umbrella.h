18687126151448b7dd675e41e0a7c35865349017
gakaki <gakaki@gmail.com>
2016-10-19 13:52:22 +0800

    commit all

---
+++ Example/Pods/Target Support Files/wow-vendor-ui/wow-vendor-ui-umbrella.h
@@ -0,0 +1,15 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "DACircularProgressView.h"
+#import "DALabeledCircularProgressView.h"
+#import "FlexboxLayout.h"
+#import "Layout.h"
+#import "KMPlaceholderTextView.h"
+#import "TagListView.h"
+#import "UICollectionViewLeftAlignedLayout.h"
+
+FOUNDATION_EXPORT double wow_vendor_uiVersionNumber;
+FOUNDATION_EXPORT const unsigned char wow_vendor_uiVersionString[];
+
---
+++ Example/wow-vendor-ui/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  wow-vendor-ui
+//
+//  Created by gakaki on 09/26/2016.
+//  Copyright (c) 2016 gakaki. All rights reserved.
+//
+
+import UIKit
+import wow_vendor_ui
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
